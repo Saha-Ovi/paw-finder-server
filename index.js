2344b693aa7e116d6a8addd4fe92b4e5f32d3287
Saha-Ovi <ovisaha3210@gmail.com>
2026-06-12 20:45:31 +0600

    Mongodb added and verfied

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const express = require("express");
 const cors = require('cors');
 const dotenv = require('dotenv');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 dotenv.config();
@@ -14,5 +14,29 @@ const PORT = process.env.PORT||5000;
 app.use(express.json());
 app.use(cors());
 
+const uri =process.env.MONGODB_URI;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+
+    const db = client.db("pawfinder");
+    const petCollection = db.collection("pets");
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
