37025d2a37415ff721838d3a792826eb7ad30b3b
Saha-Ovi <ovisaha3210@gmail.com>
2026-06-12 20:23:17 +0600

    cors express and dotenv added and checked

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require("express");
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT||5000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
 
 
